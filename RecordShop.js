a3b5b8f233760008e2656d398d827775e0721290
Josephine Elder <parusmontanus@gmail.com>
2016-12-03 14:05:37 +0000

    RecordShop passes contructor tests

---
+++ RecordShop.js
@@ -0,0 +1,12 @@
+
+var RecordShop = function(name, location) {
+    this.name = name;
+    this.location = location;
+    this.stock = [];
+    this.balance = 500;
+}
+
+
+
+
+module.exports = RecordShop;
