cb8b132610cb103f11dd3030a8db3006c0681a69
Josephine Elder <parusmontanus@gmail.com>
2016-12-03 14:04:01 +0000

    Wrote RecordShop constructor tests

---
+++ specs/record_shop_spec.js
@@ -0,0 +1,48 @@
+var assert = require('assert');
+var RecordShop = require('../RecordShop.js');
+var Record = require('../Record.js');
+
+describe('Record Shop', function() {
+
+    var rollerRecords;
+    var nevermind;
+    var stoosh;
+    var dogManStar;
+    var elastica;
+
+    beforeEach(function() {
+        rollerRecords = new RecordShop('Roller Records', 'Edinburgh');
+        nevermind = new Record('Nevermind', 'Nirvana', 11.29);
+        stoosh = new Record('Stoosh', 'Skunk Anansie', 10.99);
+        dogManStar = new Record('Dog Man Star', 'Suede', 11.99);
+        elastica = new Record('Elastica', 'Elastica', 9.99);
+    });
+
+    it('should have a name', function() {
+        assert.equal('Roller Records', rollerRecords.name);
+    });
+    it('should have a location', function() {
+        assert.equal('Edinburgh', rollerRecords.location);
+    });
+    it('should have no opening stock', function() {
+        assert.equal(0, rollerRecords.stock.length);
+    });
+    it('should have an opening cash balance of £500', function() {
+        assert.equal(500, rollerRecords.balance);
+    });
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
